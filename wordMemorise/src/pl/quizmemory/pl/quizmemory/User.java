8f30447941938bf61cc1dc20db87cffeba98eaa2
Antoni Walach <walachantoni@gmail.com>
2023-05-09 00:11:20 +0200

    Class User

---
+++ wordMemorise/src/pl/quizmemory/pl/quizmemory/User.java
@@ -0,0 +1,13 @@
+package pl.quizmemory;
+
+public class User {
+    private String login;
+    private String password;
+
+    public User(String login, String password) {
+
+        this.login = login;
+        this.password = password;
+    }
+
+}
